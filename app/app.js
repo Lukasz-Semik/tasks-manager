56b366fb31fcb86266f85f9b4fb00a347063ee1f
djpluki <djpluki@gmail.com>
2017-09-30 13:04:21 +0200

    project configuration and setup

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TasksManager from './src/components/TasksManager';
+
+import 'normalize.css/normalize.css';
+import './styles/main.scss';
+
+ReactDOM.render(<TasksManager />, document.getElementById('root'));
---
+++ app/src/components/TasksManager.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TasksManager extends Component {
+  render(){
+    return(
+      <div>
+        Connected with Main Component
+      </div>
+    );
+  }
+}
+
+export default TasksManager;
