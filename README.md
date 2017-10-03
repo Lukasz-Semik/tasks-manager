5145a1b36837f5caa73ce4ec4f408c4f8e91196a
djpluki <djpluki@gmail.com>
2017-10-03 17:46:10 +0200

    deployment build

---
+++ README.md
@@ -0,0 +1,9 @@
+# tasks-manager
+Simple tasks manager project written in react with redux.
+## Online version:
+http://memory-color-game.herokuapp.com/
+### For development:
+1.Clone this repo <br/>
+2.cd tasks-manager <br/>
+3.npm install or yarn install or yarn <br/>
+5.yarn run dev-server
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
-var path = require('path');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-  entry: "./app/index.js",
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module:{
-    rules:[
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
-};
+module.exports = (env) => {
+  const isProduction = env === 'production'
+  const CSSExtract = new ExtractTextPlugin('main.css');
+
+  return {
+    entry: "./app/index.js",
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules:[
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {sourceMap: true}
+              },
+              {
+                loader: 'sass-loader',
+                options: {sourceMap: true}
+              }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+    }
+  };
+}
